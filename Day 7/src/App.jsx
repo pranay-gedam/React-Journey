ebaf740162f6331531a52abb4ce48f8b7f1a5ede
Pranay Gedam <pranayg176@gmail.com>
2025-08-12 23:17:45 +0530

    Day 7: Event Handeling

--- Day 7/src/App.jsx
+++ Day 7/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const [name, setName] = useState("Pranay")
+  const [form, setForm] = useState({email: "", phone: ""})
+
+  const handleClick = () => {
+    alert("Hey I am clicked");
+  };
+
+  const handleMouseOver = () => {
+    alert("Hey I am a mouse over");
+  };
+
+  const handleChange = (e) => {
+    // setName (e.target.value)
+    setForm({...form, [e.target.name]:e.target.value})
+    console.log(form)
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="button">
+        <button onClick={handleClick}>Click Me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* <div className="red" onMouseOver={handleMouseOver}>
+        I am a red div
+      </div> */}
+
+      <input type="text" name="email" value={form.email?form.email:""} onChange={handleChange}/>
+      <input type="text" name="phone" value={form.phone?form.phone:""} onChange={handleChange}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
