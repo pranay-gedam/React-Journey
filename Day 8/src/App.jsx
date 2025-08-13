598845f7903a100004f8f1273030460619605103
Pranay Gedam <pranayg176@gmail.com>
2025-08-14 00:23:02 +0530

    Day 8: Build Todo App

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Navbar/>
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100">
-        <div className="addTodo">
-          <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" />
-          <button>Add</button>
+      <Navbar />
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input type="text" className="w-1/2" />
+          <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6">Add</button>
         </div>
-          <h2 className='text-lg font-bold'>Your Todos</h2>
-          <div className="todos">
-            <div className="todo flex">
-              <div className="text">
-                <div className="buttons">
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </div>
-              </div>
+        <h2 className="text-lg font-bold">Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </div>
+            <div className="buttons">
+              <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Edit</button>
+              <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Delete</button>
             </div>
           </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
