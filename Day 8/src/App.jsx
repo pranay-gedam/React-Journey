a3fc6908ac36fbe71f96829844582d201050ae15
Pranay Gedam <pranayg176@gmail.com>
2025-08-13 23:56:41 +0530

    Day 8: Build Todo App

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Navbar/>
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" />
+          <button>Add</button>
+        </div>
+          <h2 className='text-lg font-bold'>Your Todos</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
