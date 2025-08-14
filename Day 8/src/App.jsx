ff15f4adf2c71804792693ce449e74eb01bcc17d
Pranay Gedam <pranayg176@gmail.com>
2025-08-15 01:56:06 +0530

    Day 8: Build Todo App

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -2,23 +2,46 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const handleEdit = () => {};
+  const handleEdit = (e, id) => {
+    let t = todos.filter((i) => i.id === id);
+    setTodo(t[0].todo);
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  };
 
-  const handleDelete = () => {};
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  };
 
   const handleAdd = () => {
-    setTodos([...todos, { todo, isCompleted: false }]);
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
     setTodo("");
-    console.log(todos)
+    console.log(todos);
   };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
+  };
+
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id === id;
+    });
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
   };
 
   return (
@@ -34,34 +57,51 @@ function App() {
             className="w-1/2"
           />
           <button
-            onClick={handleAdd()}
+            onClick={handleAdd}
             className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
           >
-            Add
+            Save
           </button>
         </div>
         <h2 className="text-lg font-bold">Your Todos</h2>
         <div className="todos">
+          {todos.length === 0 && <div className="m-5">No Todos to display</div>}
           {todos.map((item) => {
-
-
-            return<div className="todo flex">
-              <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
-              <div className="buttons">
-                <button
-                  onClick={handleEdit()}
-                  className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={handleDelete()}
-                  className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                >
-                  Delete
-                </button>
+            return (
+              <div
+                key={item.id}
+                className="todo flex w-1/4 my-3 justify-between"
+              >
+                <div className="flex gap-5">
+                  <input
+                    name={item.id}
+                    onChange={handleCheckbox}
+                    type="checkbox"
+                    value={item.isCompleted}
+                    id=""
+                  />
+                </div>
+                <div className={item.isCompleted ? "line-through" : ""}>
+                  {item.todo}
+                </div>
+                <div className="buttons">
+                  <button
+                    onClick={(e) => handleEdit(e, item.id)}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      handleDelete(e, item.id);
+                    }}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>;
+            );
           })}
         </div>
       </div>
