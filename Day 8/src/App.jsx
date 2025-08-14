24636c440d96e39c38143a941cff1a1a295d46d2
Pranay Gedam <pranayg176@gmail.com>
2025-08-14 19:24:30 +0530

    Day 8: Build Todo App

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -4,7 +4,22 @@ import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  const handleAdd = () => {
+    setTodos([...todos, { todo, isCompleted: false }]);
+    setTodo("");
+    console.log(todos)
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
 
   return (
     <>
@@ -12,20 +27,42 @@ function App() {
       <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
         <div className="addTodo my-5">
           <h2 className="text-lg font-bold">Add a Todo</h2>
-          <input type="text" className="w-1/2" />
-          <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6">Add</button>
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            className="w-1/2"
+          />
+          <button
+            onClick={handleAdd()}
+            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+          >
+            Add
+          </button>
         </div>
         <h2 className="text-lg font-bold">Your Todos</h2>
         <div className="todos">
-          <div className="todo flex">
-            <div className="text">
-              Lorem ipsum dolor sit amet consectetur adipisicing.
-            </div>
-            <div className="buttons">
-              <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Edit</button>
-              <button className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Delete</button>
-            </div>
-          </div>
+          {todos.map((item) => {
+
+
+            return<div className="todo flex">
+              <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+              <div className="buttons">
+                <button
+                  onClick={handleEdit()}
+                  className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={handleDelete()}
+                  className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>;
+          })}
         </div>
       </div>
     </>
