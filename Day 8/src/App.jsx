9f9af561cb3a4ff7801f263096e29662e900b939
Pranay Gedam <pranayg176@gmail.com>
2025-08-15 20:17:06 +0530

    Day 8: Build Todo App

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from "uuid";
 
@@ -71,7 +73,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] w-1/2">
+      <div className="mx -3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
       <h1 className="font-bold text-center text-xl">iTask - Manage your  todos at one place </h1>
         <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className="text-lg font-bold">Add a Todo</h2>
@@ -88,7 +90,7 @@ function App() {
             Save
           </button>
         </div>
-        <input onChange={toggleFinished} type="checkbox" checked={showFinished}/> Show Finished
+        <input className="my-4" onChange={toggleFinished} type="checkbox" checked={showFinished}/> Show Finished
         <h2 className="text-lg font-bold">Your Todos</h2>
         <div className="todos">
           {todos.length === 0 && <div className="m-5">No Todos to display</div>}
@@ -96,7 +98,7 @@ function App() {
             return (showFinished || !item.isCompleted) &&
               <div
                 key={item.id}
-                className="todo flex w-1/2 my-3 justify-between"
+                className="todo flex md:w-1/2 my-3 justify-between"
               >
                 <div className="flex gap-5">
                   <input
@@ -115,7 +117,7 @@ function App() {
                     onClick={(e) => handleEdit(e, item.id)}
                     className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
                   >
-                    Edit
+                    <FaEdit />
                   </button>
                   <button
                     onClick={(e) => {
@@ -123,7 +125,7 @@ function App() {
                     }}
                     className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
                   >
-                    Delete
+                    <AiFillDelete />
                   </button>
                 </div>
               </div>
