b2154341f9c1c92de3bb6e9d278b938014634b82
Pranay Gedam <pranayg176@gmail.com>
2025-08-16 19:03:56 +0530

    Day 9: Learned Routing in React

--- Day 9/src/Components/Navbar.jsx
+++ Day 9/src/Components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <nav>
       <ul>
-        <Link to="/"> <li>Home</li></Link>
-        <Link to="/about"> <li>About</li></Link>
-        <Link to="/login"> <li>Login</li></Link>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/"> <li>Home</li></NavLink>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/about"> <li>About</li></NavLink>
+        <NavLink className={(e)=>{return e.isActive?"red": "" }} to="/login"> <li>Login</li></NavLink>
       </ul>
-    </div>
+    </nav>
   )
 }
 
