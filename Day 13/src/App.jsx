ac7450d5143bd6a941d4e02101f814a9c814786d
Pranay Gedam <pranayg176@gmail.com>
2025-08-17 20:18:41 +0530

    Day 13: Learned Forms Handeling + Connecting React to Express Backend

--- Day 13/src/App.jsx
+++ Day 13/src/App.jsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useForm } from "react-hook-form"
 
+  
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  const delay = (d)=>{
+    return new Promise((resolve, reject)=>{
+      setTimeout(() => {
+        resolve();
+      }, d * 1000);
+    })
+  }
+
+  const onSubmit = async(data) => {
+    await delay(2);   // Simulating Network Delay
+    console.log(data)
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {isSubmitting && <div>Loading...</div>}
+      <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input placeholder='Username' {...register("username", { required: {value: "true", message: "This fiels is required"}, minLength: {value: 3, message: "Min length is 3"}, maxLength: {value: 8, message: "Max length is 8"} })} type="text" />
+          {errors.username && <div className='red'>{errors.username.message}</div>}
+          <br />
+          <input placeholder='Password' {...register("password")} type="password" />
+          <br />
+          <input disabled={isSubmitting} type="submit" value="Submit" />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
