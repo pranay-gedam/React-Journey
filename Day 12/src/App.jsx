c631cb0c2831de6de2bd4843fa63631499e55cdc
Pranay Gedam <pranayg176@gmail.com>
2025-08-17 19:09:46 +0530

    Day 12: Learned useCallback() Hook

--- Day 12/src/App.jsx
+++ Day 12/src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import { useCallback } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [adjective, setAdjective] = useState("good")
+
+  // const getAdjective = () => {
+  //   return "another" + count
+  // }
+
+  const getAdjective = useCallback (()=> {
+    return "another" + count
+  }, [count])
 
   return (
     <>
+    <Navbar adjective={"good"} getAdjective={getAdjective}/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
