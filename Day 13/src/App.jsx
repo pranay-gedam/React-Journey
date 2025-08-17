ca92e3b42d3321aeecb29d49e5db8bbf3ceee963
Pranay Gedam <pranayg176@gmail.com>
2025-08-17 23:24:49 +0530

    Day 13: Learned Forms Handeling + Connecting React to Express Backend

--- Day 13/src/App.jsx
+++ Day 13/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const {
     register,
     handleSubmit,
-    watch,
+    setError,
     formState: { errors, isSubmitting },
   } = useForm()
 
@@ -19,10 +19,17 @@ function App() {
     })
   }
 
-  const onSubmit = async(data) => {
-    await delay(2);   // Simulating Network Delay
-    console.log(data)
-
+  const onSubmit = async (data) => {
+    // await delay(2);   // Simulating Network Delay
+    let r = await fetch("http://localhost:3000/", { method: "POST", headers: {"Content-Type": "application/json",}, body: JSON.stringify(data)})
+    let res = await r.text()
+    console.log(data, res)
+    // if (data.username !== "pranay") {
+    //   setError("myform", {message: "Your form is not in good order because credentials are invalid"})
+    // } 
+    // if (data.username === "prajwal") {
+    //   setError("blocked", {message: "Sorry this user is blocked"})
+    // }
   }
 
   return (
@@ -33,9 +40,12 @@ function App() {
           <input placeholder='Username' {...register("username", { required: {value: "true", message: "This fiels is required"}, minLength: {value: 3, message: "Min length is 3"}, maxLength: {value: 8, message: "Max length is 8"} })} type="text" />
           {errors.username && <div className='red'>{errors.username.message}</div>}
           <br />
-          <input placeholder='Password' {...register("password")} type="password" />
+          <input placeholder='Password' {...register("password", {minLength: {value: 7, message: "Min length of password is 7"},})} type="password" />
+          {errors.password && <div className='red'>{errors.password.message}</div>}
           <br />
           <input disabled={isSubmitting} type="submit" value="Submit" />
+          {errors.myform && <div className='red'>{errors.myform.message}</div>}
+          {errors.blocked && <div className='red'>{errors.blocked.message}</div>}
         </form>
       </div>
     </>
